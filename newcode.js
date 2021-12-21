b0835c89a94faaaaba93e21ed70855565d2b7e40
Logan Someone <loganqqq@outlook.com>
2021-12-21 10:58:10 -0500

    Add morte card edits

--- newcode.js
+++ newcode.js
@@ -6,10 +6,33 @@ function redirect(requestDetails) {
   console.info(requestDetails.url);
   if (requestDetails.url == "https://undercards.net/images/cards/Killer_Cook.png") {
     return {
-        redirectUrl: "Https://undercards.net/images/cards/Ultimate_Recipe.png"
+        redirectUrl: "https://undercards.net/images/cards/Ultimate_Recipe.png"
       };
   }
-  return;
+  else if (requestDetails.url == "https://undercards.net/images/cards/Sans.png") {
+    return {
+        redirectUrl: "https://undercards.net/images/cards/Final_Attack.png"
+      }; 
+  }
+  else if (requestDetails.url == "https://undercards.net/images/cards/Gerson.png") {
+      return {
+          redirectUrl: "https://undercards.net/images/cards/Gem_Specialist.png"
+      };
+  }
+  else if (requestDetails.url == "https://undercards.net/images/cards/Mad_Dummy.png") {
+      return {
+          redirectUrl: "https://undercards.net/images/cards/Foolish.png"
+      };
+  }
+  else if (requestDetails.url == "https://undercards.net/images/cards/Gaster.png") {
+        return {
+            redirectUrl: "https://undercards.net/images/cards/Message_from_the_Void.png"
+        };}
+    else if (requestDetails.url == "https://undercards.net/images/cards/Asriel_Dreemurr.png") {
+        return {
+            redirectUrl: "https://undercards.net/images/cards/Cute_Dictator.png"
+        };}
+  else return;
 }
 
 browser.webRequest.onBeforeRequest.addListener(
